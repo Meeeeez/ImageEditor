5687d94a7f98df1fa5c99ed650802b3966a0894c
Moritz Messner <stmesmor@bx.fallmerayer.it>
2021-04-15 08:05:58 +0200

    Code conventions, Y Image (YUV)

--- app.py
+++ app.py
@@ -4,61 +4,69 @@ from PIL import Image
 import time
 import keyboard
 
-# the [x, y] for each right-click event will be stored here
-left_clicks = list()
 
-
-def getRGBValue():
-    print("Coordinates:" + str(left_clicks))
+def get_rgb_value(clicks):
+    # print("Coordinates:" + str(left_clicks))
     image = PIL.Image.open(path_image)
     image_rgb = image.convert("RGB")
-    rgb_pixel_value = image_rgb.getpixel(left_clicks[0])
-    print("Pixel 1 RGB:" + str(rgb_pixel_value))
-    YUVfromRGB(rgb_pixel_value[0], rgb_pixel_value[1], rgb_pixel_value[2])
-    rgb_pixel_value2 = image_rgb.getpixel(left_clicks[1])
-    print("Pixel 2 RGB:" + str(rgb_pixel_value2))
-    YUVfromRGB(rgb_pixel_value2[0], rgb_pixel_value2[1], rgb_pixel_value2[2])
+    white_rgb = image_rgb.getpixel(clicks[0])
+    print("Pixel 1 RGB:" + str(white_rgb))
+    yuv_from_rgb(white_rgb[0], white_rgb[1], white_rgb[2])
+    white_balance(white_rgb)
 
 
-def YUVfromRGB(r, g, b):
+def rgb_to_y():
+    y_img = Image.open("img/takenPictureY.jpg")
+    y_pixels = y_img.load()
+    for i in range(y_img.size[0]):  # for every pixel:
+        for j in range(y_img.size[1]):
+            y_red = y_pixels[i, j][0]
+            y_green = y_pixels[i, j][1]
+            y_blue = y_pixels[i, j][2]
+            y = y_red * .299000 + y_green * .587000 + y_blue * .114000
+            y = int(round(y))
+            y_pixels[i, j] = (y, y, y)
+    print("test")
+    y_img.save("img/yPicture.jpg")
+    open_image("img/yPicture.jpg", "Y Image")
+
+
+def yuv_from_rgb(r, g, b):
     y = r * .299000 + g * .587000 + b * .114000
     u = r * -.168736 + g * -.331264 + b * .500000 + 128
     v = r * .500000 + g * -.418688 + b * -.081312 + 128
-    print("YUV: (" + str(y) + " " + str(u) + " " + str(v) + ")")
+    y = int(round(y))
+    u = int(round(u))
+    v = int(round(v))
+    # print("lol:" + str(r * .299000) + " " + str(g * .587000) + " " + str(b * .114000))
+    # print("YUV: (" + str(y) + " " + str(u) + " " + str(v) + ")")
 
 
-# this function will be called whenever the mouse is right-clicked
+# this function will be called whenever the mouse is left-clicked
 def mouse_callback(event, x, y, flags, params):
-    # right-click event value is 2
+    # left-click event value is 1
     if event == 1:
-        global left_clicks
-
-        # store the coordinates of the right-click event
+        left_clicks = list()
+        # store the coordinates of the left-click event
         left_clicks.append((x, y))
 
-        # this just verifies that the mouse data is being collected
-        # you probably want to remove this later
-        global img
-        radius = 3
-        thickness = -1
-        if len(left_clicks) % 2 == 1:
-            img = cv2.imread("img/takenPicture.jpg")
-            center_coordinates = (left_clicks[0])
-            color = (255, 0, 0)
-            img = cv2.circle(img, center_coordinates, radius, color, thickness)
-            cv2.imshow('image', img)
-        if len(left_clicks) % 2 == 0:
-            center_coordinates = (left_clicks[1])
-            color = (255, 0, 0)
-            img = cv2.circle(img, center_coordinates, radius, color, thickness)
-            cv2.imshow('image', img)
+        # global img
+        # radius = 3
+        # thickness = -1
 
-        if len(left_clicks) == 2:
-            getRGBValue()
-            left_clicks.clear()
+        # if len(left_clicks) % 2 == 0:
+        #    center_coordinates = (left_clicks[1])
+        #    color = (255, 0, 0)
+        #    img = cv2.circle(img, center_coordinates, radius, color, thickness)
+        #    cv2.imshow('image', img)
+
+        # if len(left_clicks) == 1:
+        get_rgb_value(left_clicks)
+        left_clicks.clear()
+        rgb_to_y()
 
 
-def takePictureAndSave():
+def take_picture_and_save():
     global frame
     cv2.namedWindow("Camera", cv2.WINDOW_AUTOSIZE)
     camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -69,9 +77,9 @@ def takePictureAndSave():
     while cv2.getWindowProperty('Camera', 0) >= 0:  # show image as long as window is open
         cv2.imshow("Camera", frame)
         return_value, frame = camera.read()
-        keyCode = cv2.waitKey(1)
+        key_code = cv2.waitKey(1)
 
-        if keyCode == 27:
+        if key_code == 27:
             break
 
         if keyboard.is_pressed(' '):  # take picture and save it if space is pressed
@@ -85,9 +93,36 @@ def takePictureAndSave():
     cv2.destroyWindow("Camera")
 
 
-def openImage():
+def white_balance(white_rgb):
+    img_to_change = Image.open("img/takenPicture.jpg")
+    img_to_change.save("img/takenPictureY.jpg")
+    pixels = img_to_change.load()  # create the pixel map
+    for i in range(img_to_change.size[0]):  # for every pixel:
+        for j in range(img_to_change.size[1]):
+            img_red = pixels[i, j][0]
+            img_green = pixels[i, j][1]
+            img_blue = pixels[i, j][2]
+            lum = (white_rgb[0] + white_rgb[1] + white_rgb[2]) / 3
+            if white_rgb[0] == 0:
+                white_rgb = (1, white_rgb[1], white_rgb[2])
+            if white_rgb[1] == 0:
+                white_rgb = (white_rgb[0], 1, white_rgb[2])
+            if white_rgb[2] == 0:
+                white_rgb = (white_rgb[0], white_rgb[1], 1)
+            img_red = img_red * lum / white_rgb[0]
+            img_green = img_green * lum / white_rgb[1]
+            img_blue = img_blue * lum / white_rgb[2]
+
+            pixels[i, j] = (int(img_red), int(img_green), int(img_blue))
+
+    img_to_change.save("img/modifiedPicture.jpg")
+    open_image("img/modifiedPicture.jpg", "Modified Image")
+
+
+def open_image(path, name):
+    print("lol")
     global path_image
-    path_image = "img/takenPicture.jpg"
+    path_image = path
     global img
     img = cv2.imread(path_image, None)
     scale_width = 640 / img.shape[1]
@@ -95,17 +130,18 @@ def openImage():
     scale = min(scale_width, scale_height)
     window_width = int(img.shape[1] * scale)
     window_height = int(img.shape[0] * scale)
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', window_width, window_height)
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(name, window_width, window_height)
 
-    # set mouse callback function for window
-    cv2.setMouseCallback('image', mouse_callback)
+    if name == "Taken Image":
+        # set mouse callback function for window
+        cv2.setMouseCallback(name, mouse_callback)
 
-    cv2.imshow('image', img)
+    cv2.imshow(name, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    takePictureAndSave()
-    openImage()
+    take_picture_and_save()
+    open_image("img/takenPicture.jpg", 'Taken Image')
